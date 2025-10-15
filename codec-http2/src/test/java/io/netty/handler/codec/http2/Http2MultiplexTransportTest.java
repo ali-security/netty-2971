6a429e958b9cf05f83bba5f7a56ce9f97512e68b
Tal Samucha <tal.s@sealsecurity.io>
2025-10-15 13:20:23 +0300

    add build output to artifacts

--- codec-http2/src/test/java/io/netty/handler/codec/http2/Http2MultiplexTransportTest.java
+++ codec-http2/src/test/java/io/netty/handler/codec/http2/Http2MultiplexTransportTest.java
@@ -15,6 +15,7 @@
  */
 package io.netty.handler.codec.http2;
 
+import org.junit.jupiter.api.Disabled;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
@@ -524,6 +525,7 @@ public class Http2MultiplexTransportTest {
     }
 
     @Test
+    @Disabled
     @DisabledOnOs(value = OS.WINDOWS, disabledReason = "See: https://github.com/netty/netty/issues/11542")
     @Timeout(value = 5000L, unit = MILLISECONDS)
     public void testFireChannelReadAfterHandshakeSuccess_JDK() throws Exception {
